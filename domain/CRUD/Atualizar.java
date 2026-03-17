46d0374d3aee92b3b64298c17850711d0fc02d34
joao <jvictorbicker@gmail.com>
2026-03-17 09:03:19 -0300

    added Update on CRUD

--- domain/CRUD/Atualizar.java
+++ domain/CRUD/Atualizar.java
@@ -1,9 +1,53 @@
 package CRUD;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Atualizar {
 
-    public void atualizar(String user, String name, int idade) {
+    public static void atualizar(String user, String newUser, String name, int idade) {
+        try {
+            Gson gson = new Gson();
 
+            //le o arquivo
+            FileReader reader = new FileReader("usuarios.json");
+            Type listaJson = new TypeToken<List<Usuario>>(){}.getType();
+            List <Usuario> createUser = gson.fromJson(reader, listaJson);
+
+            reader.close();
+
+            if (createUser == null) {
+                createUser = new ArrayList<>();
+            }
+
+            for (int i = 0; i < createUser.size(); i++) {
+                Usuario u = createUser.get(i);
+                if (u.getUser().equals(user)) {
+                    u.setUser(newUser);
+                    u.setName(name);
+                    u.setIdade(idade);
+                    break;
+                }
+            }
+
+            FileWriter writer = new FileWriter("usuarios.json");
+            gson.toJson(createUser, writer);
+
+            writer.close();
+
+        } catch (Exception e) {
+            System.out.println("impossivel atualizar");
+        }
+    }
+
+    public static void main(String[] args) {
+        Atualizar.atualizar("xo.victor", "victorr", "joaoo", 19);
     }
 
 }
--- domain/CRUD/Usuario.java
+++ domain/CRUD/Usuario.java
@@ -4,11 +4,29 @@ public class Usuario {
 
     public String user;
     public String name;
-    public int CPF;
+    public int idade;
 
-    public Usuario(String user, String name,  int CPF) {
+    public Usuario(String user, String name,  int idade) {
         this.user = user;
         this.name = name;
-        this.CPF = CPF;
+        this.idade = idade;
+    }
+    public String getUser() {
+        return user;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getIdade() {
+        return this.idade;
+    }
+    public void setUser(String newUser) {
+        this.user = user;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 }
